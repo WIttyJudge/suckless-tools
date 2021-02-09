38118669940976bccb1fb9b298b4a5c1fa664ef2
Ruslan <kros8925@gmail.com>
2021-02-09 07:19:47 +0000

    Auto Commit

--- dmenu-5.0/config.h
+++ dmenu-5.0/config.h
@@ -1,29 +1,20 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-/* DMENU-CENTER PATCH */
-
 /* -b  option; if 0, dmenu appears at bottom     */
 static int topbar = 1;
-
 /* -c option; centers dmenu on screen */
-static int centered = 8;
-
+static int centered = 0;
 /* minimum width when centered */
-static int min_width = 500;
-
-/* DMENU-BORDER PATCH */
-
-/* Size of the window border */
-static const unsigned int border_width = 2;
+static int min_width = 550;
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=13"
+	"monospace:size=12"
 };
 
 /* -p  option; prompt to the left of input field */
-static const char *prompt      = NULL;      
+static const char *prompt = NULL;
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
@@ -31,11 +22,18 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 25;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 2;
