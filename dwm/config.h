29e37408226f811650399ca356766edc52e23874
Ruslan <kros8925@gmail.com>
2021-02-20 16:48:15 +0300

    Auto Commit

--- dwm/config.h
+++ dwm/config.h
@@ -1,15 +1,20 @@
-/* See LICENSE file for copyright and license details. */
-
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 3;       /* snap pixel */
+
+/* dwm-vanitygaps patch */
+static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int barpadh   = 2;        /* padding height to add to te status bar */
 static const unsigned int barpadw   = 1;        /* padding width to add around text on the status bar */
 
-static const char *fonts[]          = { "monospace:pixelsize=12:antialias=true:autohint=true",
+static const char *fonts[]          = { "monospace:pixelsize=12",
 	                                    "JoyPixels:size=10:antialias=true:autohint=true" };
 
 static const char dmenufont[]       = "UbuntuMono Nerd Font:size=10";
@@ -25,14 +30,13 @@ static const char col_orange[]      = "#d08770";
 
 static const char *colors[][3]      = {
 	/*					fg         bg          border   */
-	[SchemeNorm] =	 { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  =	 { col_gray4, col_cyan, col_cyan },
-	[SchemeRed] =	 { col_red, col_gray1, col_gray2 },
-	[SchemeYellow]=	 { col_yellow, col_gray1, col_gray2 },
-	[SchemeOrange]=  { col_orange, col_gray1, col_gray2},
-	[SchemeGreen]=   { col_green, col_gray1, col_gray2},
-	[SchemeBlue]=    { col_cyan, col_gray1, col_gray2},
-	  
+	[SchemeNorm]   = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]    = { col_gray4, col_cyan, col_cyan },
+	[SchemeRed]    = { col_red, col_gray1, col_gray2 },
+	[SchemeYellow] = { col_yellow, col_gray1, col_gray2 },
+	[SchemeOrange] = { col_orange, col_gray1, col_gray2},
+	[SchemeGreen]  = { col_green, col_gray1, col_gray2},
+	[SchemeBlue]   = { col_cyan, col_gray1, col_gray2},
 };
 
 /* tagging */
@@ -43,9 +47,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/*  class      instance    title       tags mask     isfloating   monitor */
-	{ "Chromium",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/*  class      instance    title                  tags mask      iscentered     isfloating   monitor */
+	{ "Chromium",    NULL,   "Chromium Browser",       1 << 1,          0,              0,          -1 },
+	{ "Brave",       NULL,   "Brave Browser",          1 << 1,          0,              0,          -1 },
+
+	{ "Telegram",    NULL,       NULL,                 1 << 3,          1,              1,          -1 },
+	{ "Discord",     NULL,       NULL,                 1 << 3,          0,              0,          -1 },
+ 
+	{ "Steam",       NULL,       NULL,                 1 << 4,          0,              0,          -1 },
 };
 
 /* layout(s) */
@@ -71,9 +80,8 @@ static const Layout layouts[] = {
 	{ MODKEY|Mod1Mask,              KEY,      tagnextmon,     {.ui = 1 << TAG} }, \
 	{ MODKEY|Mod1Mask|ShiftMask,    KEY,      tagprevmon,     {.ui = 1 << TAG} },
 
-#define CMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define CMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
@@ -81,8 +89,8 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	/* { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, */
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	/* { MODKEY,                    XK_p,      spawn,          {.v = dmenucmd } }, */
+	/* { MODKEY,                       XK_b,      togglebar,      {0} }, */
 
 	{ MODKEY|ShiftMask,             XK_l,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_h,      rotatestack,    {.i = -1 } },
@@ -102,7 +110,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },
+	{ MODKEY|ControlMask,		        XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
@@ -113,10 +121,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY|Mod1Mask,              XK_space,  spawn,          CMD("rofi -modi drun -show drun -show-icons") },
-	{ MODKEY|Mod1Mask,				      XK_w,      spawn,          CMD("chromium") },
-	{ MODKEY|Mod1Mask,              XK_f,      spawn,          CMD("pcmanfm") },
 	{ MODKEY|Mod1Mask,              XK_c,      spawn,          CMD("rofi -show emoji -modi emoji") },
-	{ MODKEY,                       XK_x,      spawn,          CMD("~/bin/powerspec") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
