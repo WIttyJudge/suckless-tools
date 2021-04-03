a743bfd2fb817d5c4f652b0a2c888c017152b4c0
Ruslan <kros8925@gmail.com>
2021-04-04 00:50:01 +0300

    Auto Commit

---
+++ dmenu/config.h
@@ -0,0 +1,47 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+/* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;
+/* -c option; centers dmenu on screen */
+static int centered = 0;
+/* minimum width when centered */
+static int min_width = 550;
+
+static const char *fonts[] = {
+	"monospace:size=12"
+	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
+};
+
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
+
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+static const unsigned int alphas[SchemeLast][2] = {
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
+};
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 25;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
